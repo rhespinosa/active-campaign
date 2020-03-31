33e918537ef5fdec7465b696767aa8a6ff5028bb
Raul Espinosa <raul.espinosa@beora.com>
2020-03-31 17:16:37 -0600

    first commit

---
+++ lib/active/campaign.rb
@@ -0,0 +1,8 @@
+require "active/campaign/version"
+
+module Active
+  module Campaign
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/active/campaign/version.rb
@@ -0,0 +1,5 @@
+module Active
+  module Campaign
+    VERSION = "0.1.0"
+  end
+end
